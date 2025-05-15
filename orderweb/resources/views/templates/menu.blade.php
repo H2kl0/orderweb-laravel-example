9e5e4c4a1c4dfbbde5ee61f25b0c947fe6d67328
JUAN FERNANDO VELASQUEZ <juanchibiris80@gmail.com>
2025-05-15 12:48:05 -0500

    Inicio de vistas causal y mas
    Se agrega sbadmin2 como templates, se creo la vista index y create causal

--- orderweb/resources/views/templates/menu.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <div class="vertical-menu">
-        <a href="{{ route('test') }}">test</a>
-        <a href="{{ route('test2') }}">test2</a>
-    </div>
-</body>
-</html>
--- orderweb/routes/web.php
+++ orderweb/routes/web.php
@@ -17,7 +17,15 @@ Route::get('/', function () {
     return view('test');
 })->name('test');
 
-Route::get('/test2', function () {
+
+Route::get('/', function () {
     return view('test2');
 })->name('test2');
 
+Route::get('/causal/create', function () {
+    return view('causal.create');
+})->name('causal.create');
+
+Route::get('/causal/index', function () {
+    return view('causal.index');
+})->name('causal.index');
